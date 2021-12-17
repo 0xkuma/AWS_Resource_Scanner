5a1b51b1499b2593d23b50d8f024f37f66233389
eddie.cheung <>
2021-12-17 09:19:53 +0800

    change vpc rt tgw

--- app.py
+++ app.py
@@ -7,22 +7,32 @@ from openpyxl.styles import Alignment
 
 region_mapping = {"ap-southeast-1": 'Singapore', "ap-east-1": 'Hong Kong'}
 
+# Route table variables
+route_header_row = 2
+vpc_row = 2
+route_table_row = 2
+route_row = 2
 
-def get_vpc_data(role, region):
-    ws = wb.create_sheet("VPC")
-    column_list = ['VPC Name', 'CIDR', 'VPC ID']
-    ws.append(column_list)
+# Transit Gateway variables
+transit_gateway_header_row = 2
+transit_gateway_center_row = 2
+transit_gateway_row = 2
 
+
+def get_vpc_data(role, accoutnId, region, process_type):
+    ws = wb.worksheets[sheet_list.index(process_type) + 1]
     client = boto3.client('ec2', region_name=region, aws_access_key_id=role['AccessKeyId'], aws_secret_access_key=role['SecretAccessKey'],
                           aws_session_token=role['SessionToken'])
     response = client.describe_vpcs()
-    vpc_list = []
+    vpc_name = ''
+
     for vpc in response['Vpcs']:
-        vpc_name = vpc['Tags']
-        for tag in vpc_name:
-            if tag['Key'] == 'Name':
-                ws.append([tag['Value'], vpc['CidrBlock'], vpc['VpcId']])
-    return vpc_list
+        if 'Tags' in vpc:
+            for tag in vpc['Tags']:
+                if tag['Key'] == 'Name':
+                    vpc_name = tag['Value']
+        ws.append([vpc_name, vpc['CidrBlock'],
+                   vpc['VpcId'], accoutnId, region_mapping[region]])
 
 
 def get_subnet_data(role, region):
@@ -41,7 +51,12 @@ def get_subnet_data(role, region):
                           subnet['AvailabilityZone'], subnet['SubnetId']])
 
 
-def get_route_table_data(role, accountId, region):
+def get_route_table_data(role, accountId, region, process_type):
+
+    global route_header_row
+    global vpc_row
+    global route_table_row
+    global route_row
 
     def get_target_id(route):
         if 'GatewayId' in route:
@@ -59,17 +74,13 @@ def get_route_table_data(role, accountId, region):
         elif 'VpcEndpointId' in route:
             return route['VpcEndpointId']
 
-    ws = wb.create_sheet('Route Table')
-    column_list = ['Region', 'Environment', 'Account', 'VPC ID', 'Route Table Name', 'Route Table ID',
-                   'Destination', 'Target']
-    ws.append(column_list)
-
+    ws = wb.worksheets[sheet_list.index(process_type) + 1]
     client = boto3.client('ec2', region_name=region, aws_access_key_id=role['AccessKeyId'], aws_secret_access_key=role['SecretAccessKey'],
                           aws_session_token=role['SessionToken'])
     response = client.describe_route_tables()
 
     output = {'total': 0}
-
+    name = ''
     for route_table in response['RouteTables']:
         for tag in route_table['Tags']:
             if tag['Key'] == 'Name':
@@ -97,47 +108,50 @@ def get_route_table_data(role, accountId, region):
     # print(json.dumps(output, indent=4))
 
     # Write to Excel
+    if output['total'] > 0:
+        # Region Column
+        ws.merge_cells(start_row=route_header_row, start_column=1,
+                       end_row=route_header_row + output['total'] - 1, end_column=1)
+        ws.cell(row=route_header_row,
+                column=1).value = region_mapping[region]
+        # Environment Column
+        ws.merge_cells(start_row=route_header_row, start_column=2,
+                       end_row=route_header_row + output['total'] - 1, end_column=2)
+        ws.cell(row=route_header_row, column=2).value = environment
+        # Account Column
+        ws.merge_cells(start_row=route_header_row, start_column=3,
+                       end_row=route_header_row + output['total'] - 1, end_column=3)
+        ws.cell(row=route_header_row, column=3).value = accountId
+        route_header_row += output['total']
 
-    # Region Column
-    ws.merge_cells(start_row=2, start_column=1,
-                   end_row=1 + output['total'], end_column=1)
-    ws.cell(row=2, column=1).value = region_mapping[region]
-    # Environment Column
-    ws.merge_cells(start_row=2, start_column=2,
-                   end_row=1 + output['total'], end_column=2)
-    ws.cell(row=2, column=2).value = environment
-    # Account Column
-    ws.merge_cells(start_row=2, start_column=3,
-                   end_row=1 + output['total'], end_column=3)
-    ws.cell(row=2, column=3).value = accountId
-
-    vpc_row = 2
-    route_table_row = 2
-    route_row = 2
-    for vpc_id in output:
-        if vpc_id == 'total':
-            continue
-        ws.merge_cells(start_row=vpc_row, start_column=4,
-                       end_row=vpc_row+output[vpc_id]['total'] - 1, end_column=4)
-        ws.cell(row=vpc_row, column=4).value = vpc_id
-        vpc_row += output[vpc_id]['total']
-        for route_table in output[vpc_id]:
-            if route_table == 'total':
+        for vpc_id in output:
+            if vpc_id == 'total':
                 continue
-            ws.merge_cells(start_row=route_table_row, start_column=5,
-                           end_row=route_table_row+output[vpc_id][route_table]['total'] - 1, end_column=5)
-            ws.cell(row=route_table_row,
-                    column=5).value = output[vpc_id][route_table]['name']
-            ws.merge_cells(start_row=route_table_row, start_column=6,
-                           end_row=route_table_row+output[vpc_id][route_table]['total'] - 1, end_column=6)
-            ws.cell(row=route_table_row,
-                    column=6).value = route_table
-            route_table_row += output[vpc_id][route_table]['total']
-            for route in output[vpc_id][route_table]['routes']:
-                ws.cell(row=route_row,
-                        column=7).value = route['DestinationCidrBlock']
-                ws.cell(row=route_row, column=8).value = get_target_id(route)
-                route_row += 1
+            ws.merge_cells(start_row=vpc_row, start_column=4,
+                           end_row=vpc_row+output[vpc_id]['total'] - 1, end_column=4)
+            ws.cell(row=vpc_row, column=4).value = vpc_id
+            vpc_row += output[vpc_id]['total']
+            for route_table in output[vpc_id]:
+                if route_table == 'total':
+                    continue
+                ws.merge_cells(start_row=route_table_row, start_column=5,
+                               end_row=route_table_row+output[vpc_id][route_table]['total'] - 1, end_column=5)
+                ws.cell(row=route_table_row,
+                        column=5).value = output[vpc_id][route_table]['name']
+                ws.merge_cells(start_row=route_table_row, start_column=6,
+                               end_row=route_table_row+output[vpc_id][route_table]['total'] - 1, end_column=6)
+                ws.cell(row=route_table_row,
+                        column=6).value = route_table
+                route_table_row += output[vpc_id][route_table]['total']
+                for route in output[vpc_id][route_table]['routes']:
+                    if 'DestinationCidrBlock' in route:
+                        ws.cell(row=route_row,
+                                column=7).value = route['DestinationCidrBlock']
+                    elif 'DestinationPrefixListId' in route:
+                        ws.cell(row=route_row,
+                                column=7).value = route['DestinationPrefixListId']
+                    ws.cell(row=route_row, column=8).value = get_target_id(route)
+                    route_row += 1
 
 
 def get_igw_data(role, accountId, region):
@@ -283,15 +297,15 @@ def get_vpn_data(role, accountId,  region):
                        vpn_location, vpn_environment, accountId, vpn_no, vpn_name, "", "", attached_name, customer_gateway_name, customer_gateway_address, public_ip_address, asn, "", virtual_private_gateway])
 
 
-def get_tgw_data(role, region):
-    ws = wb.create_sheet('Transit Gateway')
-    column_list = ['Region', 'Environment', 'Transit Gateway route table ID', 'Transit Gateway Route Table Name', "CIDR",
-                   "Transit-GW Attachment ID", "Resource type", "ID (VPC/Direct Connect Gateway/VPN)",
-                   "Route type", "Route state"]
-    ws.append(column_list)
+def get_tgw_data(role, accountId, region, process_type):
 
-    output = {}
+    global transit_gateway_header_row
+    global transit_gateway_center_row
+    global transit_gateway_row
 
+    output = {"total": 0}
+
+    ws = wb.worksheets[sheet_list.index(process_type) + 1]
     client = boto3.client('ec2', region_name=region, aws_access_key_id=role['AccessKeyId'], aws_secret_access_key=role['SecretAccessKey'],
                           aws_session_token=role['SessionToken'])
 
@@ -305,50 +319,66 @@ def get_tgw_data(role, region):
                 ]
             }])['Routes'])
 
+        tgw_name = ''
+        if 'Tags' in tgw:
+            for tag in tgw['Tags']:
+                if tag['Key'] == 'Name':
+                    tgw_name = tag['Value']
+
         output[tgw['TransitGatewayRouteTableId']] = {
-            "name": [tag["Value"] for tag in tgw['Tags'] if tag["Key"] == 'Name'][0],
+            "name": tgw_name,
             "routes": routes
         }
+        output['total'] += len(routes)
+    if output['total'] > 0:
+        for item in output:
+            if item == 'total':
+                continue
 
-    merge_row = 2
-    row = 2
+            ws.merge_cells(start_row=transit_gateway_center_row, end_row=transit_gateway_center_row +
+                           len(output[item]['routes'])-1, start_column=4,  end_column=4)
+            ws.cell(row=transit_gateway_center_row, column=4).value = item
+            ws.merge_cells(start_row=transit_gateway_center_row, end_row=transit_gateway_center_row +
+                           len(output[item]['routes'])-1, start_column=5,  end_column=5)
+            ws.cell(row=transit_gateway_center_row,
+                    column=5).value = output[item]['name']
+            transit_gateway_center_row += len(output[item]['routes'])
 
-    for item in output:
-        ws.merge_cells(start_row=merge_row, end_row=merge_row +
-                       len(output[item]['routes'])-1, start_column=3,  end_column=3)
-        ws.cell(row=merge_row, column=3).value = item
-        ws.merge_cells(start_row=merge_row, end_row=merge_row +
-                       len(output[item]['routes'])-1, start_column=4,  end_column=4)
-        ws.cell(row=merge_row, column=4).value = output[item]['name']
-        merge_row += len(output[item]['routes'])
-
-        for route in output[item]['routes']:
-            ws.cell(
-                row=row, column=5).value = route['DestinationCidrBlock']
-
-            if 'TransitGatewayAttachments' in route:
+            for route in output[item]['routes']:
                 ws.cell(
-                    row=row, column=6).value = route['TransitGatewayAttachments'][0]['TransitGatewayAttachmentId']
+                    row=transit_gateway_row, column=6).value = route['DestinationCidrBlock']
+
+                if 'TransitGatewayAttachments' in route:
+                    ws.cell(
+                        row=transit_gateway_row, column=7).value = route['TransitGatewayAttachments'][0]['TransitGatewayAttachmentId']
+                    ws.cell(
+                        row=transit_gateway_row, column=8).value = route['TransitGatewayAttachments'][0]['ResourceType']
+                    ws.cell(
+                        row=transit_gateway_row, column=9).value = route['TransitGatewayAttachments'][0]['ResourceId']
+
                 ws.cell(
-                    row=row, column=7).value = route['TransitGatewayAttachments'][0]['ResourceType']
+                    row=transit_gateway_row, column=10).value = route['Type']
                 ws.cell(
-                    row=row, column=8).value = route['TransitGatewayAttachments'][0]['ResourceId']
+                    row=transit_gateway_row, column=11).value = route['State']
+                transit_gateway_row += 1
 
-            ws.cell(
-                row=row, column=9).value = route['Type']
-            ws.cell(
-                row=row, column=10).value = route['State']
-            row += 1
-
-    ws.merge_cells(start_row=2, end_row=row - 1, start_column=1,  end_column=1)
-    ws.cell(row=2, column=1).value = region_mapping[region]
-    ws.cell(row=2, column=1).alignment = Alignment(
-        horizontal='center', vertical='center')
-
-    ws.merge_cells(start_row=2, end_row=row - 1, start_column=2,  end_column=2)
-    ws.cell(row=2, column=2).value = environment
-    ws.cell(row=2, column=2).alignment = Alignment(
-        horizontal='center', vertical='center')
+        ws.merge_cells(start_row=transit_gateway_header_row,
+                       end_row=transit_gateway_header_row+output['total'] - 1, start_column=1,  end_column=1)
+        ws.cell(row=transit_gateway_header_row,
+                column=1).value = region_mapping[region]
+        ws.cell(row=transit_gateway_header_row, column=1).alignment = Alignment(
+            horizontal='center', vertical='center')
+        ws.merge_cells(start_row=transit_gateway_header_row,
+                       end_row=transit_gateway_header_row+output['total'] - 1, start_column=2,  end_column=2)
+        ws.cell(row=transit_gateway_header_row, column=2).value = environment
+        ws.cell(row=transit_gateway_header_row, column=2).alignment = Alignment(
+            horizontal='center', vertical='center')
+        ws.merge_cells(start_row=transit_gateway_header_row,
+                       end_row=transit_gateway_header_row+output['total'] - 1, start_column=3,  end_column=3)
+        ws.cell(row=transit_gateway_header_row, column=3).value = accountId
+        ws.cell(row=transit_gateway_header_row, column=3).alignment = Alignment(
+            horizontal='center', vertical='center')
+        transit_gateway_header_row += output['total']
 
 
 if __name__ == '__main__':
@@ -356,28 +386,45 @@ if __name__ == '__main__':
 
     with open('./setting.yaml', 'r') as f:
         scan_list = yaml.load(f, Loader=yaml.BaseLoader)
+    wb = Workbook()
+    ws = wb.active
 
-    for account in scan_list:
-        # assume role
-        sts = boto3.client('sts')
-        role = sts.assume_role(
-            RoleArn='arn:aws:iam::{}:role/OrganizationAccountAccessRole'.format(
-                account),
-            RoleSessionName='AWSCLI-Session'
-        )['Credentials']
+    sheet_list = ['VPC', 'Route Table', 'Transit Gateway']
 
-        for region in scan_list[account]['region']:
-            wb = Workbook()
-            ws = wb.active
-            # get_vpc_data()
+    def header(item):
+        return {
+            'VPC': ['VPC Name', 'CIDR',
+                    'VPC ID', "Account ID", "Region"],
+            'Route Table': ['Region', 'Environment', 'Account', 'VPC ID', 'Route Table Name', 'Route Table ID',
+                            'Destination', 'Target'],
+            "Transit Gateway": ['Region', 'Environment', 'Account', 'Transit Gateway route table ID', 'Transit Gateway Route Table Name', "CIDR",
+                                "Transit-GW Attachment ID", "Resource type", "ID (VPC/Direct Connect Gateway/VPN)",
+                                "Route type", "Route state"]
+        }[item]
+
+    for item in sheet_list:
+        ws = wb.create_sheet(item)
+        ws.append(header(item))
+
+    for region in scan_list:
+        for account in scan_list[region]:
+            print(region, account)
+            # assume role
+            sts = boto3.client('sts')
+            role = sts.assume_role(
+                RoleArn='arn:aws:iam::{}:role/OrganizationAccountAccessRole'.format(
+                    account),
+                RoleSessionName='AWSCLI-Session'
+            )['Credentials']
+            get_vpc_data(role, account, region, 'VPC')
             # get_subnet_data()
-            # get_route_table_data(role, account, region)
+            get_route_table_data(role, account, region, 'Route Table')
             # get_igw_data(role, account, region)
             # get_endpoint_data()
             # get_endpoint_service_data()
             # get_nat_data()
             # get_sg_data()
-            get_vpn_data(role, account, region)
-            # get_tgw_data(role, region)
-            wb.save('{}_{}_resource.xlsx'.format(
-                account, region_mapping[region]))
+            # get_vpn_data(role, account, region)
+            get_tgw_data(role, account, region, 'Transit Gateway')
+            print('-----Finish-----')
+    wb.save('output/output.xlsx')
